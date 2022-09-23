1ada01959e5a64c4ca703d5a0ee9eb2aa31d246f
Scott Moore <scott@moore.cloud>
2022-09-23 13:36:38 -0500

    added changes
    -added credit to AWS for original tool
    -comment lines on where to add

--- sync.sh
+++ sync.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# modified by somoore
+
+#This your path to aws/config, edit as needed
 PROFILEFILE="$HOME/.aws/config"
+
+#This your path to steampipe connections for aws, edit as needed
 CONNECTIONFILE="$HOME/.steampipe/config/aws.spc"
 #PROFILEFILE="~/.aws/config"
 #CONNECTIONFILE="~/.aws/aws.spc"
@@ -22,6 +43,7 @@ CONNECTIONFILE="$HOME/.steampipe/config/aws.spc"
 #fi
 
 profilefile=${PROFILEFILE};
+#Add your AWS SSO Start URL on the next line
 START_URL="https://[start-url].awsapps.com/start#/";
 REGION="us-east-1";
 
